a1534692bb12d748e8176335eef7923ed3b10a36
Tyler Smith <tjismith13@icloud.com>
2023-06-28 11:53:32 -0400

    Change back to original source filtering

--- backend/libraries/elasticsearch/data.go
+++ backend/libraries/elasticsearch/data.go
@@ -218,9 +218,11 @@ func GetAlarms(s *state.State, indices []string, sources []string, start time.Ti
 	r := elastic.NewRangeQuery("timestamp").
 		From(start.Format(time.RFC3339)).
 		To(end.Format(time.RFC3339))
-	// query for all alarms in range and filter for origSource being in alarmSources
+	// query for all alarms in range and filter for either origSource or respSource being in alarmSources
 	origSources := elastic.NewTermsQuery("id_orig_h_pos", alarmSources...)
-	query := elastic.NewBoolQuery().Must(r).Must(origSources)
+	respSources := elastic.NewTermsQuery("id_resp_h_pos", alarmSources...)
+	hasSource := elastic.NewBoolQuery().Should(origSources, respSources)
+	query := elastic.NewBoolQuery().Must(r).Must(hasSource)
 	queryResult, err := client.Search().Index(indices...).
 		Query(query).Sort("timestamp", false).Size(size).From(from).Do(ctx)
 	if err != nil {
